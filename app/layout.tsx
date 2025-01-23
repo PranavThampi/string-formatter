9b7c1273d16c4a704865644e7d808ca773660b07
Pranav Thampi <pranav.thampi@vivvix.com>
2025-01-23 17:40:03 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,26 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { CSPostHogProvider } from "./providers";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "String Formatter",
-  description: "Format your list of strings with custom delimiters and wrapping characters.",
-}
+  description:
+    "Format your list of strings with custom delimiters and wrapping characters.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>{children}</body>
+      <CSPostHogProvider>
+        <body className={inter.className}>{children}</body>
+      </CSPostHogProvider>
     </html>
-  )
+  );
 }
-
